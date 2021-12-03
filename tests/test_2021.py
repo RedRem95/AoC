71d8f60a191b99fd54031a1f2cb66a42c80d157f
RedRem95 <admin@example.com>
2021-12-03 08:54:28 +0100

    Implemented Day03

--- tests/test_2021.py
+++ tests/test_2021.py
@@ -1,7 +1,8 @@
 import pytest
 
 from AoC_Companion.Day import StarTask
-from y2021 import Day01, Day02
+from y2021 import Day01, Day02, Day03
+
 
 def test_day01():
     day = Day01.Day01(year=2021)
@@ -26,6 +27,21 @@ def test_day02():
     expected = {
         StarTask.Task01: "150",
         StarTask.Task02: "900"
+    }
+
+    for task, expected_data in expected.items():
+        res = day.run(task=task, data=data)
+        if res is not None:
+            res = res.get_result()
+        assert res == expected_data, f"{day.get_year()} - {day.get_name()} - {task.name} failed"
+
+def test_day03():
+    day = Day03.Day03(year=2021)
+    data = day.construct_data_package(data="00100\n11110\n10110\n10111\n10101\n01111\n00111\n11100\n10000\n11001\n00010\n01010")
+
+    expected = {
+        StarTask.Task01: "198",
+        StarTask.Task02: "230"
     }
 
     for task, expected_data in expected.items():
