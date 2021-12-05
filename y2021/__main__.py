37f4d9b6a1e99d8c00391559e839b894b64e98d7
RedRem95 <31988414+redrem95@users.noreply.github.com>
2021-12-05 09:26:41 +0100

    Argument to run all

--- y2021/__main__.py
+++ y2021/__main__.py
@@ -1,6 +1,16 @@
-from . import aoc
+import argparse
+
 from AoC_Companion.Day import TaskResult
 
+from . import aoc
+
 if __name__ == "__main__":
-    results = aoc.run_latest()
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('-a', "--all", action="store_true", dest="all", help="Decision if you want to run all days")
+    args = parser.parse_args()
+
+    if args.all:
+        results = aoc.run()
+    else:
+        results = aoc.run_latest()
     print(TaskResult.format(*results, show_log=True))
