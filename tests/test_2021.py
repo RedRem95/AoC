ba62bca5da02b2976c1e51a13550f4cdb8edecfd
RedRem95 <31988414+redrem95@users.noreply.github.com>
2021-12-05 09:26:55 +0100

    Implemented Day05

--- tests/test_2021.py
+++ tests/test_2021.py
@@ -2,7 +2,7 @@ import os
 
 from AoC_Companion.Day import StarTask
 
-from y2021 import Day01, Day02, Day03, Day04
+from y2021 import Day01, Day02, Day03, Day04, Day05
 
 
 def test_day01():
@@ -62,6 +62,23 @@ def test_day04():
     expected = {
         StarTask.Task01: 4512,
         StarTask.Task02: 1924
+    }
+
+    for task, expected_data in expected.items():
+        res = day.run(task=task, data=data)
+        if res is not None:
+            res = res.get_result()
+        assert res == expected_data, f"{day.get_year()} - {day.get_name()} - {task.name} failed"
+
+
+def test_day05():
+    day = Day05.Day05(year=2021)
+    with open(os.path.join(os.path.dirname(__file__), "test_resources", "day05.txt"), "rb") as fin:
+        data = day.construct_data_package(data=fin.read().decode("utf-8"))
+
+    expected = {
+        StarTask.Task01: 5,
+        StarTask.Task02: 12
     }
 
     for task, expected_data in expected.items():
