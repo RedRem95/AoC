19e04f7839698edbe8e1cfb2c5c16a6db48b3341
RedRem95 <31988414+redrem95@users.noreply.github.com>
2021-12-26 11:51:29 +0100

    changed used method back to faster variant

--- y2021/Day24/__init__.py
+++ y2021/Day24/__init__.py
@@ -23,9 +23,9 @@ class Day24(Day):
 
     def run_t1(self, data: List[str]) -> Optional[TaskResult]:
         monad = MONAD(instruction_lines=data)
-        # valid_numbers = monad.find_valid_numbers()
-        biggest_num = monad.force_biggest_number()
-        return TaskResult(biggest_num)
+        valid_numbers = monad.find_valid_numbers()
+        # biggest_num = monad.force_biggest_number()
+        return TaskResult((valid_numbers[0], valid_numbers[-1]))
 
     def run_t2(self, data: Any) -> Optional[TaskResult]:
         return TaskResult(None)
