891ae5505a1662bf587e134ef6c096a34737ca1c
RedRem95 <redrem_github@botschmot.de>
2022-12-01 06:44:57 +0100

    Added file template to git

---
+++ .idea/fileTemplates/AoC Day.py
@@ -0,0 +1,24 @@
+from typing import Callable, AnyStr
+
+from AoC_Companion.Day import Task
+from AoC_Companion.test import TestData
+from AoC_Companion.Preprocess import Preprocessor
+
+
+@Preprocessor(year=${WHAT_YEAR}, day=${WHAT_DAY})
+def preproc_1(data):
+    # process data
+    return data
+
+
+@Task(year=${WHAT_YEAR}, day=${WHAT_DAY}, task=1)
+def task01(data, log: Callable[[AnyStr], None]):
+    # create the result for day 1 task 1
+    log("Some very interesting and useful logs")
+    return res
+
+
+@Task(year=${WHAT_YEAR}, day=${WHAT_DAY}, task=2)
+def task02(data, log: Callable[[AnyStr], None]):
+    # create the result for day 1 task 2
+    return res
