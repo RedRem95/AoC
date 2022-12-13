7868e25154e3d1dce83b55c344080c3965d5d766
RedRem95 <redrem@redrem95.de>
2022-12-13 20:56:18 +0100

    Implemented automatic import of year code

--- AoC/__init__.py
+++ AoC/__init__.py
@@ -1,2 +1,7 @@
-from .y2021 import *
-from .y2022 import *
+import os
+from importlib import import_module
+
+for k in os.listdir(os.path.dirname(__file__)):
+    if not os.path.isdir(os.path.join(os.path.dirname(__file__), k)) or k.startswith("_"):
+        continue
+    import_module(f".{k}", __name__)
